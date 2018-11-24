74cde4fa8d5a0faf81b87deebb6f9cf85136fa2d
Ziv Haroety <ziv@haroety.com>
2018-11-24 16:51:58 +0200

    creating junits.

---
+++ tests/src/FutureTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+public class FutureTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void get() {
+    }
+
+    @org.junit.Test
+    public void resolve() {
+    }
+
+    @org.junit.Test
+    public void isDone() {
+    }
+
+    @org.junit.Test
+    public void get1() {
+    }
+}
---
+++ tests/src/InventoryTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getInstance() {
+    }
+
+    @org.junit.Test
+    public void load() {
+    }
+
+    @org.junit.Test
+    public void take() {
+    }
+
+    @org.junit.Test
+    public void checkAvailabiltyAndGetPrice() {
+    }
+
+    @org.junit.Test
+    public void printInventoryToFile() {
+    }
+}
